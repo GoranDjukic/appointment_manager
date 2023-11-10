361023bc7cb6f09901bfe8267fdb6a0afe7b0924
GoranDjukic <gorandjukicdev@gmail.com>
2023-11-10 12:35:36 +0100

    Initial commit

---
+++ src/test/java/com/gdjukic/appointmentManager/AppointmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdjukic.appointmentManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
